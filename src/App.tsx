e0e3d6dec80f3e1458a5bab71f4b55f6bedc334a
Marcio Vale <mvale@smartdigit.pt>
2023-09-12 18:09:17 +0100

    Add shadcn components Create main layout Add dark colors to index.css Install separator, textarea and button shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,51 @@
+import {Github} from "lucide-react"
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+
 export function App() {
 
   return (
-   <h1>Upload.AI</h1>
+   <div className="min-h-screen flex flex-col">
+    <div className="px-6 py-3 flex items-center justify-between border-b">
+      <h1 className="text-xl font-bold">upload.ai</h1>
+
+      <div className="flex items-center gap-3">
+        <span className="text-sm text-muted-foreground">Desenvolvido com 💟 no NLW da Rockeseat</span>
+
+        <Separator orientation="vertical" className="h-6"/>
+
+        <Button
+          variant="outline"
+        >
+            <Github className="w-4 h-4 mr-2" />
+            Github
+        </Button>
+      </div>
+    </div>
+
+    <main className="flex-1 p-6 flex gap-6">
+      <div className="flex flex-col flex-1 gap-4">
+        <div className="grid grid-rows-2 gap-4 flex-1">
+          <Textarea 
+            placeholder="Incluia o prompt para a IA..."
+            className="resize-none p-4 leading-relaxed"
+          />
+          <Textarea 
+            placeholder="Resultado gerado pela IA..."  
+            className="resize-none p-4 leading-relaxed"
+            readOnly
+          />
+        </div>
+
+        <p className="text-sm text-muted-foreground">
+          Lembre-se: você pode utilizar a variável <code className="text-violet-400">{`{transcription}`}</code> no seu prompt para adicionar o conteúdo da transcrição do video selecionado.</p>
+      </div>
+
+      <aside className="w-80">
+        
+      </aside>
+    </main>
+   </div>
   )
 }
